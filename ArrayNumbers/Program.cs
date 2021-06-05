f50aee101a6c665d31341762bfde7ae52ad829e2
Siarhei <taler34@gmail.com>
2021-06-05 22:26:49 +0300

    ArrayNumbers EDU

---
+++ ArrayNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindMinMax(new double[] {1.346, 1.6532, 1.8734, 1.8723}));
+        }
+
+        public static double[] FindMinMax(double[] values)
+        {
+            double[] minMax = {values.Min(), values.Max()};
+            return minMax;
+        }
+    }
+}
